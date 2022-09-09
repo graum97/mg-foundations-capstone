2624d87df56c7358fe7fe1db53dafd349431d155
Mara Grau <mngrau97@gmail.com>
2022-09-09 15:49:07 -0500

    by eod on friday

--- public/library.js
+++ public/library.js
@@ -1,9 +1,21 @@
 //grab add book form
 const newBookForm = document.querySelector("form")
-//grab to-read contatiner
 
-//grab read container
+// const titleInput = document.querySelector('input[name="title"]').value;
 
+// const authorInput = document.querySelector('input[name="author"]').value;
+
+// const genreSelection = document.querySelector('option[name="genre"]:selected').value;
+
+// const obtainSelection = document.querySelector('input[name="obtain"]:checked').value;
+
+// const readSelection = document.querySelector('input[name="read"]:checked').value;
+
+
+// //grab to-read contatiner
+// const toReadList = document.querySelector('#to-read');
+// //grab read container
+// const readList = document.querySelector('#read');
 //establish baseURL
 const baseURL = `http://localhost:6006/api/library`
 
@@ -11,9 +23,5 @@ const baseURL = `http://localhost:6006/api/library`
 
 
 
-//activate fiction button
 
-//activate literature button
-
-//activate nonfiction button
 
